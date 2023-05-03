4f1ad8d99ee5453037dea76f886e3abd09450598
ForhadRoben <i.rforhad@gmail.com>
2023-05-04 05:32:02 +0600

    chefs-info-json-data-loaded

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefsInfo = require('./data/chefs_info.json');
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World');
 });
 
+app.get('/chefsInfo', (req, res) => {
+    res.send(chefsInfo);
+})
+
 app.listen(port, () => {
-    console.log(`Recipe API is running on port: ${port}`)
+    console.log(`Recipe API is running on port: ${port}`);
 })
