efab8ef4bd10b5ccef64c625ae899e5bef3c3f46
ForhadRoben <i.rforhad@gmail.com>
2023-05-04 09:31:13 +0600

    id-details-send-to-client-side

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ app.get('/chefsInfo', (req, res) => {
     res.send(chefsInfo);
 })
 
-
+app.get('/chefsInfo/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    const singleChef = chefsInfo.find(n => parseInt(n._id) === id);
+    res.send(singleChef)
+})
 
 app.listen(port, () => {
     console.log(`Recipe API is running on port: ${port}`);
